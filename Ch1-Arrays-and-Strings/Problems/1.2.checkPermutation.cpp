d3946ee5995216819691359d25e78819e7152e0c
tw2686 <tw2686@github.com>
2020-01-11 02:30:23 -0500

    1.2 checkPermutations

--- Ch1-Arrays-and-Strings/Problems/1.2.checkPermutation.cpp
+++ Ch1-Arrays-and-Strings/Problems/1.2.checkPermutation.cpp
@@ -9,12 +9,49 @@
  * 131
  *
  * ask:
+ * 1. whether its case sensitive
+ * 2. whether whitespace is significant
+ *
+ * solutions:
+ * 1. sort each string, compare
+ * 2. count letters
  */
 
-
-bool permutations(std::string s1, std::string s2)
+/* array of ASCII booleans */
+bool permutations1(std::string s1, std::string s2)
 {
-	return false;
+	if (s1.length() != s2.length()) return false;
+
+	bool chars[256] = { false };
+
+	for (int i = 0; i < s1.length(); ++i) {
+		int v1 = (int)s1[i];
+		int v2 = (int)s2[i];
+
+		chars[v1] = !chars[v1];
+		chars[v2] = !chars[v2];
+	}
+
+	for (int i = 0; i < 256; ++i)
+		if (chars[i] == true) return false;
+
+	return true;
+}
+
+/* binary representation */
+bool permutations2(std::string s1, std::string s2)
+{
+	if (s1.length() != s2.length()) return false;
+
+	int bit1 = 0, bit2 = 0;
+
+	for (int i = 0; i < s1.length(); ++i) {
+		bit1 |= 1 << (s1[i] - 'a');
+	}
+	for (int i = 0; i < s2.length(); ++i) {
+		bit2 |= 1 << (s2[i] - 'a');
+	}
+	return (bit1 ^ bit2) == 0;
 }
 
 int main()
@@ -27,8 +64,9 @@ int main()
 		{ "", "" }
 	};
 
-	for (int i = 0; i < tests.size(); ++i) {
-		std::cout << permutations(tests[i]);
+	for (int i = 0; i < sizeof(tests)/sizeof(tests[0]); ++i) {
+		std::cout << tests[i][0] << ", " << tests[i][1] << ", ";
+		std::cout << permutations2(tests[i][0], tests[i][1]);
 		std::cout << std::endl;
 	}
 
